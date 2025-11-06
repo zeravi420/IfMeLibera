e5a5bdf1b555d470946e78d112b0e1553ccb46a0
Ze Ravier <jose.vaca@estudante.ifro.edu.br>
2025-11-06 17:13:57 -0400

    Create chanel class with properties and methods

---
+++ chanel.java
@@ -0,0 +1,17 @@
+public class chanel{
+    int numero;
+    String nome;
+    public canal(int numero, String nome){
+        this.numero = numero;
+        this.nome = nome;
+    }
+    public int getNumero(){
+        return numero;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public String toString(){
+        return numero + "-" + nome;
+    }
+}
